67ebc4cdb2058b54980076c4579f5f761e7ce71b
Frederik Berlaen <frederik@typemytype.com>
2019-05-14 16:08:50 +0200

    make it tuple while setting
    mixing defcon with fontParts objects
    
    fixes #32

--- LibExtension/glyphConstructionUI.py
+++ LibExtension/glyphConstructionUI.py
@@ -361,7 +361,7 @@ class BuildGlyphsSheet(BaseWindowController):
             construction.draw(glyph.getPen())
 
             if construction.markColor:
-                glyph.markColor = construction.markColor
+                glyph.markColor = tuple(construction.markColor)
             elif markColor:
                 glyph.markColor = markColor
 

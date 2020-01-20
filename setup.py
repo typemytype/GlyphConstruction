5c85cffaab34563c4d635f09f50e20d8cc677f13
Frederik Berlaen <frederik@typemytype.com>
2020-01-20 20:43:24 +0100

    touch version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='glyphConstruction',
-    version="0.0.2",
+    version="0.0.3",
     author='Frederik Berlaen',
     author_email='frederik@typemytpye.com',
     url='https://github.com/typemytype/GlyphConstruction',
